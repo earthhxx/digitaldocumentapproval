807539cb8f15a77427a8c1f2e6135212033da994
earthhxx <earthhwachirawit@gmail.com>
2025-08-25 09:58:26 +0700

    save

--- src/app/pages/Userlogin/D-APPROVE/components/D_approvetable.tsx
+++ src/app/pages/Userlogin/D-APPROVE/components/D_approvetable.tsx
@@ -119,12 +119,12 @@ export default function DApproveTable({ user, initialData }: DApproveTableProps)
             <h2 className="flex justify-center items-center  text-4xl font-bold mb-5 text-gray-800 mt-4">Document Approval</h2>
 
             {/* Tabs */}
-            <div className="flex mb-5 border-b border-gray-300">
+            <div className={`flex mb-5 border-b border-gray-300 `}>
                 {(Object.keys(tabLabels) as Tab[]).map((t) => (
                     <button
                         key={t}
                         onClick={() => handleTabChange(t)}
-                        className={`px-5 py-2 font-medium border-b-2 transition-colors duration-200 ${tab === t
+                        className={`px-5 py-2 font-medium border-b-2 transition-colors duration-200 ${user.permissions?.includes(t) ? "" : "hidden"} ${tab === t
                             ? "border-blue-600 text-blue-600"
                             : "border-transparent text-gray-500 hover:text-gray-700"
                             }`}
