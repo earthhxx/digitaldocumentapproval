a261f1ed500564908d51e06306c86bcc34d40a68
earthhxx <earthhwachirawit@gmail.com>
2025-08-25 10:24:24 +0700

    save

--- src/app/pages/Userlogin/D-APPROVE/components/D_approvetable.tsx
+++ src/app/pages/Userlogin/D-APPROVE/components/D_approvetable.tsx
@@ -134,150 +134,156 @@ export default function DApproveTable({ user, initialData }: DApproveTableProps)
                 ))}
             </div>
 
-
-            {/* Search */}
-            <form onSubmit={handleSearch} className="mb-5 flex gap-3 text-black">
-                <input
-                    type="text"
-                    value={search}
-                    onChange={(e) => setSearch(e.target.value)}
-                    placeholder="Search document..."
-                    className="border border-gray-300 rounded-md px-4 py-2 flex-1 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400 text-gray-700"
-                />
-
-                <button
-                    type="submit"
-                    className="bg-blue-600 text-white px-5 py-2 rounded-md hover:bg-blue-700 transition-colors"
+            {(Object.keys(tabLabels) as Tab[]).map((t) => (
+                <div
+                    key={t}
+                    className={`${user.permissions?.includes(tabLabels[t]) ? "" : "hidden"}`}
                 >
-                    Search
-                </button>
-            </form>
+                    {/* Search */}
+                    <form onSubmit={handleSearch} className="mb-5 flex gap-3 text-black">
+                        <input
+                            type="text"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            placeholder="Search document..."
+                            className="border border-gray-300 rounded-md px-4 py-2 flex-1 focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400 text-gray-700"
+                        />
 
-            {/* Totals */}
-            <div className="mb-3 text-gray-600 font-medium">
-                Total documents: <span className="text-blue-600">{approveData.totalAll}</span>
-            </div>
+                        <button
+                            type="submit"
+                            className="bg-blue-600 text-white px-5 py-2 rounded-md hover:bg-blue-700 transition-colors"
+                        >
+                            Search
+                        </button>
+                    </form>
 
-            {/* Table */}
-            <div className="overflow-x-auto h-[60vh] custom-scrollbar rounded-lg shadow-sm border border-gray-200">
-                <table className="min-w-full bg-white border-collapse text-black">
-                    <thead className="bg-gray-100 ">
-                        <tr>
-                            <th className="text-left px-4 py-2 font-medium ">ID</th>
-                            <th className="text-left px-4 py-2 font-medium ">Source</th>
-                            <th className="text-left px-4 py-2 font-medium ">Date</th>
-                            <th className="text-left px-4 py-2 font-medium w-[5%]">action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {loading ? (
-                            <tr>
-                                <td colSpan={4} className="text-center py-6 ">
-                                    Loading...
-                                </td>
-                            </tr>
-                        ) : approveData.data.length === 0 ? (
-                            <tr>
-                                <td colSpan={4} className="text-center py-6 ">
-                                    No data found
-                                </td>
-                            </tr>
-                        ) : (
-                            approveData.data.map((doc) => (
-                                <tr key={`${doc.source}-${doc.id}`} className="hover:bg-gray-50 transition-colors">
-                                    <td className="px-4 py-2 border-t border-gray-200">{doc.id}</td>
-                                    <td className="px-4 py-2 border-t border-gray-200">{doc.source}</td>
-                                    <td className="px-4 py-2 border-t border-gray-200">
-                                        {doc.date ? new Date(doc.date).toLocaleDateString() : "-"}
-                                    </td>
-                                    <td className="flex px-4 py-2 border-t border-gray-200 justify-center">
-                                        <button onClick={() => { openPDF(doc.id, doc.source); }} className="flex text-sm text-white px-4 pt-2 border-t border-gray-200 bg-blue-500 rounded-sm text-center items-center justify-center">OPEN</button>
-                                    </td>
-                                </tr>
-                            ))
-                        )}
-                    </tbody>
-                </table>
-            </div>
-
-            {/* Pagination */}
-            <div className="mt-5 flex justify-between items-center">
-                <button
-                    onClick={() => fetchData(Math.max(offset - limit, 0), search, tab)}
-                    disabled={offset === 0 || loading}
-                    className={`px-4 py-2 rounded-md ${offset === 0 || loading
-                        ? "bg-gray-200 text-gray-400 cursor-not-allowed"
-                        : "bg-blue-600 text-white hover:bg-blue-700 transition-colors"
-                        }`}
-                >
-                    Previous
-                </button>
-                <button
-                    onClick={() => fetchData(offset + limit, search, tab)}
-                    disabled={offset + limit >= approveData.totalAll || loading}
-                    className={`px-4 py-2 rounded-md ${offset + limit >= approveData.totalAll || loading
-                        ? "bg-gray-200 text-gray-400 cursor-not-allowed"
-                        : "bg-blue-600 text-white hover:bg-blue-700 transition-colors"
-                        }`}
-                >
-                    Next
-                </button>
-            </div>
-
-            {
-                showPDF && (
-                    <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex justify-center items-center z-50 p-4">
-                        <div className="relative w-full max-w-[95vw] h-[95vh] bg-white rounded-xl shadow-lg flex flex-col">
-                            <div className="absolute right-0 p-4 ">
-                                {/* <h2 className="text-lg font-semibold text-gray-800">PDF Viewer</h2> */}
-                                <button
-                                    className="text-white bg-red-600 hover:bg-red-700 rounded-full w-10 h-10 flex items-center justify-center text-2xl"
-                                    onClick={() => setShowPDF(false)}
-                                >
-                                    ✕
-                                </button>
-                            </div>
-
-                            <iframe src={pdfUrl} className="w-full h-full border-none flex-1 " title="PDF Viewer" />
-
-                            <div className="absolute bottom-4 right-4 flex gap-4">
-                                {user?.permissions?.includes("Check") && (
-                                    <button
-                                        className="px-5 py-2 bg-blue-600 text-white rounded-lg"
-                                        onClick={() => setShowSupervisorPopup(true)}
-                                    >
-                                        สำหรับหัวหน้างาน
-                                    </button>
-                                )}
-                                {user?.permissions?.includes("Appove") && (
-                                    <button
-                                        className="px-5 py-2 bg-green-600 text-white rounded-lg"
-                                        onClick={() => setShowManagerPopup(true)}
-                                    >
-                                        สำหรับผู้จัดการ
-                                    </button>
-                                )}
-                            </div>
-
-                            {showSupervisorPopup && (
-                                <SupervisorPopup
-                                    onClose={() => setShowSupervisorPopup(false)}
-                                    onApprove={() => handleApproval("", "", "", "approve", "supervisor")}
-                                    onReject={() => handleApproval("", "", "", "reject", "supervisor")}
-                                />
-                            )}
-
-                            {showManagerPopup && (
-                                <Manager
-                                    onClose={() => setShowManagerPopup(false)}
-                                    onApprove={() => handleApproval("", "", "", "approve", "manager")}
-                                    onReject={() => handleApproval("", "", "", "reject", "manager")}
-                                />
-                            )}
-                        </div>
+                    {/* Totals */}
+                    <div className="mb-3 text-gray-600 font-medium">
+                        Total documents: <span className="text-blue-600">{approveData.totalAll}</span>
                     </div>
-                )
-            }
+
+                    {/* Table */}
+                    <div className="overflow-x-auto h-[60vh] custom-scrollbar rounded-lg shadow-sm border border-gray-200 ">
+                        <table className="min-w-full bg-white border-collapse text-black">
+                            <thead className="bg-gray-100 ">
+                                <tr>
+                                    <th className="text-left px-4 py-2 font-medium ">ID</th>
+                                    <th className="text-left px-4 py-2 font-medium ">Source</th>
+                                    <th className="text-left px-4 py-2 font-medium ">Date</th>
+                                    <th className="text-left px-4 py-2 font-medium w-[5%]">action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {loading ? (
+                                    <tr>
+                                        <td colSpan={4} className="text-center py-6 ">
+                                            Loading...
+                                        </td>
+                                    </tr>
+                                ) : approveData.data.length === 0 ? (
+                                    <tr>
+                                        <td colSpan={4} className="text-center py-6 ">
+                                            No data found
+                                        </td>
+                                    </tr>
+                                ) : (
+                                    approveData.data.map((doc) => (
+                                        <tr key={`${doc.source}-${doc.id}`} className="hover:bg-gray-50 transition-colors">
+                                            <td className="px-4 py-2 border-t border-gray-200">{doc.id}</td>
+                                            <td className="px-4 py-2 border-t border-gray-200">{doc.source}</td>
+                                            <td className="px-4 py-2 border-t border-gray-200">
+                                                {doc.date ? new Date(doc.date).toLocaleDateString() : "-"}
+                                            </td>
+                                            <td className="flex px-4 py-2 border-t border-gray-200 justify-center">
+                                                <button onClick={() => { openPDF(doc.id, doc.source); }} className="flex text-sm text-white px-4 pt-2 border-t border-gray-200 bg-blue-500 rounded-sm text-center items-center justify-center">OPEN</button>
+                                            </td>
+                                        </tr>
+                                    ))
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+
+                    {/* Pagination */}
+                    <div className="mt-5 flex justify-between items-center">
+                        <button
+                            onClick={() => fetchData(Math.max(offset - limit, 0), search, tab)}
+                            disabled={offset === 0 || loading}
+                            className={`px-4 py-2 rounded-md ${offset === 0 || loading
+                                ? "bg-gray-200 text-gray-400 cursor-not-allowed"
+                                : "bg-blue-600 text-white hover:bg-blue-700 transition-colors"
+                                }`}
+                        >
+                            Previous
+                        </button>
+                        <button
+                            onClick={() => fetchData(offset + limit, search, tab)}
+                            disabled={offset + limit >= approveData.totalAll || loading}
+                            className={`px-4 py-2 rounded-md ${offset + limit >= approveData.totalAll || loading
+                                ? "bg-gray-200 text-gray-400 cursor-not-allowed"
+                                : "bg-blue-600 text-white hover:bg-blue-700 transition-colors"
+                                }`}
+                        >
+                            Next
+                        </button>
+                    </div>
+
+                    {
+                        showPDF && (
+                            <div className="fixed inset-0 bg-black/50 backdrop-blur-sm flex justify-center items-center z-50 p-4">
+                                <div className="relative w-full max-w-[95vw] h-[95vh] bg-white rounded-xl shadow-lg flex flex-col">
+                                    <div className="absolute right-0 p-4 ">
+                                        {/* <h2 className="text-lg font-semibold text-gray-800">PDF Viewer</h2> */}
+                                        <button
+                                            className="text-white bg-red-600 hover:bg-red-700 rounded-full w-10 h-10 flex items-center justify-center text-2xl"
+                                            onClick={() => setShowPDF(false)}
+                                        >
+                                            ✕
+                                        </button>
+                                    </div>
+
+                                    <iframe src={pdfUrl} className="w-full h-full border-none flex-1 " title="PDF Viewer" />
+
+                                    <div className="absolute bottom-4 right-4 flex gap-4">
+                                        {user?.permissions?.includes("Check") && (
+                                            <button
+                                                className="px-5 py-2 bg-blue-600 text-white rounded-lg"
+                                                onClick={() => setShowSupervisorPopup(true)}
+                                            >
+                                                สำหรับหัวหน้างาน
+                                            </button>
+                                        )}
+                                        {user?.permissions?.includes("Approve") && (
+                                            <button
+                                                className="px-5 py-2 bg-green-600 text-white rounded-lg"
+                                                onClick={() => setShowManagerPopup(true)}
+                                            >
+                                                สำหรับผู้จัดการ
+                                            </button>
+                                        )}
+                                    </div>
+
+                                    {showSupervisorPopup && (
+                                        <SupervisorPopup
+                                            onClose={() => setShowSupervisorPopup(false)}
+                                            onApprove={() => handleApproval("", "", "", "approve", "supervisor")}
+                                            onReject={() => handleApproval("", "", "", "reject", "supervisor")}
+                                        />
+                                    )}
+
+                                    {showManagerPopup && (
+                                        <Manager
+                                            onClose={() => setShowManagerPopup(false)}
+                                            onApprove={() => handleApproval("", "", "", "approve", "manager")}
+                                            onReject={() => handleApproval("", "", "", "reject", "manager")}
+                                        />
+                                    )}
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+            ))}
         </div >
     );
 }
