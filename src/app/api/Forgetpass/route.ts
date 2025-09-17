c6e3989b371cb5db3bba161cf6e3459b8f5af767
earthhxx <earthhwachirawit@gmail.com>
2025-09-17 14:12:06 +0700

    update if input thai in  forgetpass

--- src/app/api/Forgetpass/route.ts
+++ src/app/api/Forgetpass/route.ts
@@ -1,6 +1,9 @@
 // /pages/api/usertable/edituser.ts
 import { NextRequest, NextResponse } from "next/server";
 import { getDashboardConnection } from "@/lib/db";
+import sql from "mssql"; // ✅ ต้อง import sql มาด้วย
+
+const hasThai = (text: string) => /[\u0E00-\u0E7F]/.test(text);
 
 export async function POST(req: NextRequest) {
     try {
@@ -11,11 +14,16 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ error: "กรอกข้อมูลไม่ครบ" }, { status: 400 });
         }
 
+        // ❌ ถ้า user ใส่ภาษาไทยมา
+        if (hasThai(User_Id) || hasThai(Pass)) {
+            return NextResponse.json({ error: "ไม่อนุญาตให้ใช้ภาษาไทย" }, { status: 400 });
+        }
+
         const pool = await getDashboardConnection();
 
         // เช็คว่า User_Id มีอยู่หรือไม่
         const checkUser = await pool.request()
-            .input("User_Id", User_Id)
+            .input("User_Id", sql.VarChar, User_Id)
             .query(`
                 SELECT User_Id FROM [dbo].[tb_im_employee] WHERE User_Id=@User_Id
             `);
@@ -25,14 +33,13 @@ export async function POST(req: NextRequest) {
         }
 
         await pool.request()
-            .input("User_Id", User_Id)
-            .input("Pass", Pass)
+            .input("User_Id", sql.VarChar, User_Id)
+            .input("Pass", sql.VarChar, Pass)
             .query(`
-                        UPDATE [dbo].[tb_im_employee]
-                        SET Pass = @Pass, ForgetPass = NULL
-                        WHERE User_Id = @User_Id
-                    `);
-
+                UPDATE [dbo].[tb_im_employee]
+                SET Pass = @Pass, ForgetPass = NULL
+                WHERE User_Id = @User_Id
+            `);
 
         // SELECT ข้อมูลทั้งหมดส่งกลับ
         const result = await pool.request().query(`
